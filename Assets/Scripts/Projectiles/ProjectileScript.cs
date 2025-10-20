c592f28e79cc3b68ef6b54b66d5d33b31b8f7178
Vince <vinpressey@gmail.com>
2025-10-20 22:59:37 +0100

    Ranged Enemy Weakspots
    + Floating number spawner link

--- Assets/Scripts/Projectiles/ProjectileScript.cs
+++ Assets/Scripts/Projectiles/ProjectileScript.cs
@@ -1,25 +1,84 @@
+using TMPro;
 using UnityEngine;
 
 // By Vincent Pressey
 
 public class ProjectileScript : MonoBehaviour
 {
+    public FloatingTextSystem floatingTextSystem;
+
     /// <summary>
     /// Damage dealt to object when projectile makes contact
     /// </summary>
     public float ProjectileDamage = 12;
 
+    public TMP_ColorGradient weakSpotGradient;
+    public TMP_ColorGradient normalGradient;
+    public TMP_ColorGradient strongSpotGradient;
+
+    private bool hasHit;
+
+    private float lifeTime = 5;
+
+    private void Start()
+    {
+        Invoke("DestroyObject", lifeTime);
+    }
 
     private void OnTriggerEnter(Collider collider)
     {
-        if (collider.isTrigger) { return; }
+        if (hasHit) { return; }
+
+        if (collider.isTrigger)
+        {
+
+            EnemyDamageArea damageArea;
+            if (damageArea = collider.gameObject.GetComponent<EnemyDamageArea>()) // Damage object if it has the enemy damage area script attached
+            {
+                hasHit = true;
+
+                damageArea.TakeDamage(-ProjectileDamage);
+
+                float dmgMult = damageArea.GetMultiplier();
+                SpawnFloatingText(dmgMult);
+
+                Destroy(gameObject);
+            }
+            else { return; }
+
+        }
 
         Health healthscript;
         if (healthscript = collider.gameObject.GetComponent<Health>()) // Damage object if it has the health script attached
         {
+            print("FSUFS");
             healthscript.AddToHealth(-ProjectileDamage);
         }
 
         Destroy(gameObject);
+
     }
+
+    public void SpawnFloatingText(float damageMult)
+    {
+        TMP_ColorGradient gradient = normalGradient;
+
+        if (damageMult > 1.3f)
+        {
+            gradient = weakSpotGradient;
+        }
+        else if (damageMult < 0.7f)
+        {
+            gradient = strongSpotGradient;
+        }
+
+        floatingTextSystem.SpawnText((damageMult * ProjectileDamage).ToString("F0"), gradient, 5);
+    }
+
+
+    public void DestroyObject()
+    {
+        Destroy(gameObject);
+    }
+
 }
