2f2e471f5d69293ef152075aaa97b0185c9f00a3
Vince <vinpressey@gmail.com>
2025-10-20 18:48:13 +0100

    Prop and Barrel Spawners

---
+++ Assets/Scripts/ProceduralLevelGeneration/BarrelSpawner.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+// By Vincent Pressey
+
+// NOT YET FINISHED
+
+public class BarrelSpawner : MonoBehaviour
+{
+    /// <summary>
+    /// List of objects that be used to randomly pick from
+    /// </summary>
+    [SerializeField]
+    private GameObject[] barrelPrefabs;
+    /// <summary>
+    /// Object that will be deleted after actual model is loaded
+    /// </summary>
+    [SerializeField]
+    private GameObject temporaryPiece;
+    /// <summary>
+    /// Whether or not to randomise the Y-axis rotation
+    /// </summary>
+    [SerializeField]
+    private bool randomiseYRotation;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Choose random piece from given list then spawn at this object with same rotation
+        GameObject newobject = Instantiate(barrelPrefabs[Random.Range(0, barrelPrefabs.Length - 1)], transform.position, transform.rotation, transform);
+        if (randomiseYRotation)
+        {
+            newobject.transform.rotation = Quaternion.Euler(newobject.transform.rotation.eulerAngles.x, Random.Range(0, 359), newobject.transform.rotation.eulerAngles.z);
+        }
+
+        Destroy(temporaryPiece); // Destroy the piece used for creation
+        Destroy(this);
+    }
+
+}
