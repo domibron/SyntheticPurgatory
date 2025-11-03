28614bf16b9082fe1b907e3ec30c1c6c454c666a
domibron <dommcneill@outlook.com>
2025-11-03 17:31:40 +0000

    Weapon charge mechanics added

--- Assets/Scripts/UI/PlayerHUD.cs
+++ Assets/Scripts/UI/PlayerHUD.cs
@@ -12,8 +12,8 @@ public class PlayerHUD : MonoBehaviour
 
     GameManager gameManager;
 
-    [SerializeField]
-    private TMP_Text ammoText;
+    // [SerializeField]
+    // private TMP_Text ammoText;
 
     [SerializeField]
     private Image healthBarFill;
@@ -32,6 +32,10 @@ public class PlayerHUD : MonoBehaviour
 
     [SerializeField]
     private Image chargeCursor;
+    [SerializeField]
+    private float minAngle = -90;
+    [SerializeField]
+    private float maxAngle = -270;
 
     [SerializeField]
     private Image chargeUpSegment;
@@ -58,7 +62,7 @@ public class PlayerHUD : MonoBehaviour
         playerHealth.onHealthChanged += OnHealthChanged;
 
         gameManager = GameManager.Instance;
-        gameManager.StartTimer(); // TODO: move to level generator.
+        if (gameManager != null) gameManager.StartTimer(); // TODO: move to level generator.
 
         savedAlpha = damageVignette.color.a;
 
@@ -90,7 +94,7 @@ public class PlayerHUD : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ammoText.text = playerCombat.GetCurrentAmmo().ToString() + "/" + playerCombat.GetMaxAmmo().ToString();
+        // ammoText.text = "REMOVED MECHANIC";
         healthBarFill.fillAmount = playerHealth.GetHealthNormalized();
 
         if (GameManager.Instance != null)
@@ -100,18 +104,24 @@ public class PlayerHUD : MonoBehaviour
         damageVignette.color = new Color(damageVignette.color.a, damageVignette.color.g, damageVignette.color.b, Mathf.Lerp(0, savedAlpha, currentApearTime / apearTime));
 
 
-        float halfwidth = chargeBar.rectTransform.sizeDelta.x / 2f;
+        // float halfwidth = chargeBar.rectTransform.sizeDelta.x / 2f;
 
 
-        chargeCursor.rectTransform.localPosition = new Vector3((2f * playerCombat.GetCursorPos() - 1f) * halfwidth, 0, 0);
+        // chargeCursor.rectTransform.localPosition = new Vector3((2f * playerCombat.GetCursorPos() - 1f) * halfwidth, 0, 0);
+        chargeCursor.rectTransform.localRotation = Quaternion.Lerp(Quaternion.Euler(0, 0, minAngle), Quaternion.Euler(0, 0, maxAngle), playerCombat.GetCursorPos());
 
-        chargeUpSegment.rectTransform.sizeDelta = new Vector2(playerCombat.GetChargeUpSize() * (halfwidth * 2f), chargeUpSegment.rectTransform.sizeDelta.y);
-        chargeUpSegment.rectTransform.localPosition = new Vector3((2f * playerCombat.GetChargeUpPos() - 1f) * halfwidth, 0, 0);
+        // chargeUpSegment.rectTransform.sizeDelta = new Vector2(playerCombat.GetChargeUpSize() * (halfwidth * 2f), chargeUpSegment.rectTransform.sizeDelta.y);
+        // chargeUpSegment.rectTransform.localPosition = new Vector3((2f * playerCombat.GetChargeUpPos() - 1f) * halfwidth, 0, 0);
+        chargeUpSegment.fillAmount = (Mathf.Lerp(0, 180, playerCombat.GetChargeUpSize()) / 360f);
+        float rotationAmount = Mathf.Lerp(270, 90, playerCombat.GetChargeUpPos()) + ((360f * chargeUpSegment.fillAmount) / 2f);
+        chargeUpSegment.rectTransform.localRotation = Quaternion.Euler(0, 0, rotationAmount);
+
 
         weaponChargeBarFill.fillAmount = playerCombat.GetChargeAmount();
 
-        basicPassIndicator.rectTransform.sizeDelta = new Vector2(halfwidth, basicPassIndicator.rectTransform.sizeDelta.y);
-        basicPassIndicator.rectTransform.localPosition = new Vector3((playerCombat.IsChargeOnLeftSide() ? -halfwidth / 2f : halfwidth / 2f), 0, 0);
+        // basicPassIndicator.rectTransform.sizeDelta = new Vector2(halfwidth, basicPassIndicator.rectTransform.sizeDelta.y);
+        // basicPassIndicator.rectTransform.localPosition = new Vector3((playerCombat.IsChargeOnLeftSide() ? -halfwidth / 2f : halfwidth / 2f), 0, 0);
+        basicPassIndicator.rectTransform.localRotation = Quaternion.Euler(0, 0, (playerCombat.IsChargeOnLeftSide() ? 270 : 180));
     }
 
 
