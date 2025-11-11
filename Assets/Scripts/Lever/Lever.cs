6045299260da61b5ae7dc2212824955f0d9c9b25
domibron <dommcneill@outlook.com>
2025-11-11 01:08:08 +0000

    Added shoot lever to toggle

--- Assets/Scripts/Lever/Lever.cs
+++ Assets/Scripts/Lever/Lever.cs
@@ -1,10 +1,24 @@
 using UnityEngine;
 
-public class Lever : MonoBehaviour, IKickable
+public class Lever : MonoBehaviour, IKickable, IShootable
 {
     private int roomID = -1;
 
     private DoorGenerator doorGenerator;
+
+    public void HitObject()
+    {
+        if (roomID == -1)
+        {
+            LevelGenerator levelGen = LevelGenObjectRefGetter.Instance.gameObject.GetComponent<LevelGenerator>();
+
+            roomID = levelGen.GetRoomIDFromCoordinates(levelGen.GetGridCoordinates(transform.position));
+
+            doorGenerator = LevelGenObjectRefGetter.Instance.gameObject.GetComponent<DoorGenerator>();
+        }
+
+        doorGenerator.ToggleDoors(roomID);
+    }
 
     public void KickObject(Vector3 forceAndDir, ForceMode forceMode = ForceMode.Force)
     {
