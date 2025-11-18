784791b744a94a9dc6e60fb1a8831eb79ff8872a
domibron <dommcneill@outlook.com>
2025-11-18 03:42:54 +0000

    added lever model
    fixed player hud being off

--- Assets/Scripts/Lever/DirectLever.cs
+++ Assets/Scripts/Lever/DirectLever.cs
@@ -5,14 +5,29 @@ public class DirectLever : MonoBehaviour, IKickable, IShootable
 {
     public UnityEvent OnLeverActivate;
 
+    private Animator animator;
+
+    private bool state = false;
+
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        animator.SetBool("isOn", state);
+    }
+
     public void HitObject()
     {
         OnLeverActivate?.Invoke();
+        state = !state;
     }
 
     public void KickObject(Vector3 forceAndDir, ForceMode forceMode = ForceMode.Force)
     {
         OnLeverActivate?.Invoke();
-
+        state = !state;
     }
 }
