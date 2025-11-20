6df2de0dc8379047c2a4b39d84109586a0315d45
domibron <dommcneill@outlook.com>
2025-11-20 13:50:00 +0000

    fixed some UI positonal issues
    Fixed beta text saying blue (major issue I know)
    added some functions to the settings script.

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -1,6 +1,13 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+
+[Serializable]
+public class SettingsData
+{
+
+}
 
 public class SettingsMenu : MonoBehaviour
 {
@@ -26,6 +33,12 @@ public class SettingsMenu : MonoBehaviour
     [SerializeField]
     private GameObject controlsPage;
 
+    [Serializable]
+    public struct ScreenResolution
+    {
+        public int width;
+        public int height;
+    }
 
 
     void Awake()
@@ -35,6 +48,8 @@ public class SettingsMenu : MonoBehaviour
         pauseInput = InputSystem.actions.FindAction("Pause");
 
         pauseInput.started += KeyCloseSettings;
+
+        OpenGameplaySettings(); // want to make sure only one UI panel is up. a little reset as you will.
     }
 
     private void KeyCloseSettings(InputAction.CallbackContext context)
@@ -71,11 +86,12 @@ public class SettingsMenu : MonoBehaviour
         if (settingsCanvasCollection == null) { return; }
         if (!settingsCanvasCollection.gameObject.activeSelf) { return; }
 
-        if (pauseCanvas != null) { pauseCanvas.StartCoroutine("SettingsClosedDelay"); }
+        if (pauseCanvas != null) { pauseCanvas.StartCoroutine(pauseCanvas.SettingsClosedDelay()); }
 
         settingsCanvasCollection.SetActive(false);
     }
 
+    // TODO: replace with enum system instead.
     public void OpenGameplaySettings()
     {
         gameplayPage.SetActive(true);
@@ -107,5 +123,34 @@ public class SettingsMenu : MonoBehaviour
         soundPage.SetActive(false);
         controlsPage.SetActive(true);
     }
+
+
+    // Actaul settings
+
+    public void ChangeResolution(ScreenResolution resolution)
+    {
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreenMode, Screen.currentResolution.refreshRateRatio);
+    }
+
+    public void ChangeRefreshRate(RefreshRate refreshRate)
+    {
+        Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, Screen.fullScreenMode, refreshRate);
+    }
+
+    public void ChangeFullScreenMode(FullScreenMode fullScreenMode)
+    {
+        Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, fullScreenMode, Screen.currentResolution.refreshRateRatio);
+    }
+
+    public void SetVsyncEnabled(bool enableVSync = false, bool ifEnableUseHalfVSync = false)
+    {
+        QualitySettings.vSyncCount = (enableVSync ? (ifEnableUseHalfVSync ? 2 : 1) : 0);
+    }
+
+    public void SetTargetFrameRate(int targetFrameRate = -1)
+    {
+        Application.targetFrameRate = targetFrameRate;
+    }
+
 
 }
