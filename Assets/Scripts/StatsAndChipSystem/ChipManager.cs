9bb5b43a873c76429f0026158651798adceedf50
domibron <dommcneill@outlook.com>
2026-01-20 06:32:13 +0000

    grid is now working
    just need data syncing, stats updating and open cards page and some other polish

--- Assets/Scripts/StatsAndChipSystem/ChipManager.cs
+++ Assets/Scripts/StatsAndChipSystem/ChipManager.cs
@@ -28,7 +28,7 @@ public class ChipManager : MonoBehaviour
 
     int[,] chipBoard = new int[3, 4]; // Hard coded size, grid on the chip menu on hub does not update to fit this!
 
-    List<int> allActiveChips = new List<int>();
+    Dictionary<int, Vector2Int> allPlacedChips = new Dictionary<int, Vector2Int>();
     List<int> allInventoryChips = new List<int>();
 
     void Awake()
@@ -41,22 +41,65 @@ public class ChipManager : MonoBehaviour
 
         Instance = this;
 
+        SetUpBoard();
     }
 
-    public bool AddChipToGrid(int id, Vector2Int targetPos)
+    void Start()
     {
-        allActiveChips.Add(id);
-        return allChips[id].PlaceChipAtTargetSlot(ref chipBoard, targetPos, id);
+        OpenModule(ChipType.Common);
+        OpenModule(ChipType.Common);
+    }
+
+    private void SetUpBoard()
+    {
+        for (int y = 0; y < chipBoard.GetLength(ChipSO.Y_ROW); y++)
+        {
+            for (int x = 0; x < chipBoard.GetLength(ChipSO.X_ROW); x++)
+            {
+                chipBoard[y, x] = -1;
+            }
+        }
+    }
+
+    /// <summary>
+    /// This can add a new chip or move an existing chip.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="targetPos"></param>
+    /// <returns>Whether the operation was successful.</returns>
+    public bool AddChipToBoard(int id, Vector2Int targetPos)
+    {
+        if (allPlacedChips.ContainsKey(id))
+        {
+            if (allChips[id].CanPlaceAtTargetSlot(ref chipBoard, targetPos, id))
+            {
+                allChips[id].RemoveChipFromBoard(ref chipBoard, id); // move the chip. // Remove all old cells otherwise it will be hard to remove both new and old.
+                allChips[id].PlaceChipAtTargetSlot(ref chipBoard, targetPos, id);
+                return true;
+            }
+        }
+        else
+        {
+            if (allChips[id].PlaceChipAtTargetSlot(ref chipBoard, targetPos, id))
+            {
+                allPlacedChips.Add(id, targetPos);
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public void RemoveChipFromBoard(int id)
     {
-        allActiveChips.Remove(id);
+        allPlacedChips.Remove(id);
         allChips[id].RemoveChipFromBoard(ref chipBoard, id); // this is a little silly, i know.
     }
 
     public void AddChipToInventory(int id)
     {
+        if (allInventoryChips.Contains(id)) return;
+
         allInventoryChips.Add(id);
     }
 
@@ -67,7 +110,7 @@ public class ChipManager : MonoBehaviour
 
     public void AddChipModifiers(ref PlayerStats playerStats, ref MiscellaneousStats miscellaneousStats)
     {
-        foreach (var chip in allActiveChips)
+        foreach (int chip in allPlacedChips.Keys)
         {
             allChips[chip].ModifyStats(ref playerStats, ref miscellaneousStats);
         }
@@ -105,5 +148,44 @@ public class ChipManager : MonoBehaviour
         return collection[Random.Range(0, collection.Length)];
     }
 
+    public List<int> GetAllInventoryChips()
+    {
+        return allInventoryChips;
+    }
 
+    public Dictionary<int, Vector2Int> GetAllPlacedChips()
+    {
+        return allPlacedChips;
+    }
+
+    public ChipSO GetChipDataFromID(int id)
+    {
+        if (!allChips.ContainsKey(id)) return null;
+
+        return allChips[id];
+    }
+
+    public int GetChipIdFromGridPos(Vector2Int pos)
+    {
+        if (pos.x < 0 || pos.y < 0 || pos.y >= chipBoard.GetLength(ChipSO.Y_ROW) || pos.x >= chipBoard.GetLength(ChipSO.X_ROW)) return -1;
+
+        return chipBoard[pos.y, pos.x];
+    }
+
+    public string BoardToString()
+    {
+        int[,] data = chipBoard;
+
+        string returnedString = "";
+
+        for (int r = 0; r < data.GetLength(0); r++)
+        {
+            for (int c = 0; c < data.GetLength(1); c++)
+            {
+                returnedString += data[r, c].ToString("D2") + " ";
+            }
+            returnedString += "\n";
+        }
+        return returnedString;
+    }
 }
--- Assets/Scripts/StatsAndChipSystem/ChipSOs/ChipSO.cs
+++ Assets/Scripts/StatsAndChipSystem/ChipSOs/ChipSO.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "ScriptableObjects/Stats/BaseChip", fileName = "SO_BaseChip")]
+[CreateAssetMenu(menuName = "ScriptableObjects/Chips/BaseChip", fileName = "SO_BaseChip")]
 public class ChipSO : ScriptableObject
 {
+    public const int X_ROW = 1;
+    public const int Y_ROW = 0;
+
     [SerializeField]
     Vector2Int[] blockLayout = { Vector2Int.zero };
 
@@ -35,17 +38,23 @@ public class ChipSO : ScriptableObject
         return BoardItem;
     }
 
-    public virtual bool CanPlaceAtTargetSlot(int[,] chipBoard, Vector2Int targetGridPos, int currentID = -1)
+    public virtual bool CanPlaceAtTargetSlot(ref int[,] chipBoard, Vector2Int targetGridPos, int currentID = -1)
     {
         foreach (Vector2Int chipPart in blockLayout)
         {
             Vector2Int checkingPos = targetGridPos + new Vector2Int(chipPart.x, -chipPart.y);
 
-            if (checkingPos.x < 0 || checkingPos.y < 0 || checkingPos.y >= chipBoard.GetLength(0) || checkingPos.x >= chipBoard.GetLength(1))
+            if (checkingPos.x < 0 || checkingPos.y < 0 || checkingPos.y >= chipBoard.GetLength(Y_ROW) || checkingPos.x >= chipBoard.GetLength(X_ROW))
+            {
+                // Debug.Log("out of bounds");
                 return false; // Out of bounds.
+            }
 
-            if (IsSlotOccupied(chipBoard, checkingPos, currentID))
+            if (IsSlotOccupied(ref chipBoard, checkingPos, currentID))
+            {
+                // Debug.Log("Slot is occupied");
                 return false; // Slot not available.
+            }
         }
 
         return true;
@@ -55,14 +64,14 @@ public class ChipSO : ScriptableObject
 
     public virtual bool PlaceChipAtTargetSlot(ref int[,] chipBoard, Vector2Int targetGridPos, int currentID)
     {
-        if (!CanPlaceAtTargetSlot(chipBoard, targetGridPos)) return false;
+        if (!CanPlaceAtTargetSlot(ref chipBoard, targetGridPos)) return false;
 
         foreach (Vector2Int chipPart in blockLayout)
         {
             // invert the Y because +1 y means up on screen but not on the board. Top to bottom, 0 to X.
             Vector2Int chipPartPos = targetGridPos + new Vector2Int(chipPart.x, -chipPart.y);
 
-            SetIDAtTargetSlot(ref chipBoard, targetGridPos, currentID);
+            SetIDAtTargetSlot(ref chipBoard, chipPartPos, currentID);
         }
 
         return true;
@@ -70,9 +79,9 @@ public class ChipSO : ScriptableObject
 
     public virtual void RemoveChipFromBoard(ref int[,] chipBoard, int currentID)
     {
-        for (int y = 0; y < chipBoard.GetLength(0); y++)
+        for (int y = 0; y < chipBoard.GetLength(Y_ROW); y++)
         {
-            for (int x = 0; x < chipBoard.GetLength(1); x++)
+            for (int x = 0; x < chipBoard.GetLength(X_ROW); x++)
             {
                 if (chipBoard[y, x] == currentID) chipBoard[y, x] = -1;
             }
@@ -81,17 +90,18 @@ public class ChipSO : ScriptableObject
 
 
 
-    protected bool IsSlotOccupied(int[,] chipBoard, Vector2Int targetGridPos, int currentID = -1)
+    protected bool IsSlotOccupied(ref int[,] chipBoard, Vector2Int targetGridPos, int currentID = -1)
     {
-        //0, 0 top left,
-        //0, X bottom left
-        //X, 0 top right
+        // Y, X - R, C
+        //[0, 0] top left,
+        //[X, 0] bottom left
+        //[0, X] top right
         return chipBoard[targetGridPos.y, targetGridPos.x] != -1 && chipBoard[targetGridPos.y, targetGridPos.x] != currentID;
     }
 
     protected void SetIDAtTargetSlot(ref int[,] chipBoard, Vector2Int targetGridPos, int currentID)
     {
-        if (targetGridPos.x < 0 || targetGridPos.y < 0 || targetGridPos.y >= chipBoard.GetLength(0) || targetGridPos.x >= chipBoard.GetLength(1)) return;
+        if (targetGridPos.x < 0 || targetGridPos.y < 0 || targetGridPos.y >= chipBoard.GetLength(Y_ROW) || targetGridPos.x >= chipBoard.GetLength(X_ROW)) return;
 
         chipBoard[targetGridPos.y, targetGridPos.x] = currentID;
     }
