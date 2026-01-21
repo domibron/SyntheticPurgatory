1e6c6fa1fa1d53a2455b9b898306e5a8197a1126
domibron <dommcneill@outlook.com>
2026-01-21 15:51:10 +0000

    working on new chip, optimised some code

--- Assets/Scripts/UI/HubWorld/UpgradeMenu/UpgradeMenuManager.cs
+++ Assets/Scripts/UI/HubWorld/UpgradeMenu/UpgradeMenuManager.cs
@@ -22,7 +22,7 @@ public enum StatType
     BashDelay,
     EnemyStaggerTime,
     Reach,
-    KnockbackForce,
+    KnockBackForce,
 
     // Misc
     MaxScrapCarry,
@@ -121,200 +121,23 @@ public class UpgradeMenuManager : MonoBehaviour
 
     public (UpgradablePlayerStat, UpgradablePlayerStat) GetCurrentStat(StatType statType)
     {
-        switch (statType)
-        {
-            // STATS
-            case StatType.MaxHealth:
-                return (currentPStats.MaxHealthStat, upgradedButNotAppliedPStats.MaxHealthStat);
-            case StatType.RegenerationAmount:
-                return (currentPStats.RegenerationAmountStat, upgradedButNotAppliedPStats.RegenerationAmountStat);
-            case StatType.GroundSpeed:
-                return (currentPStats.GroundSpeedStat, upgradedButNotAppliedPStats.GroundSpeedStat);
-            case StatType.SlideBoostForce:
-                return (currentPStats.SlideBoostPercentageStat, upgradedButNotAppliedPStats.SlideBoostPercentageStat);
-            case StatType.AirBoostForce:
-                return (currentPStats.AirBoostPercentageStat, upgradedButNotAppliedPStats.AirBoostPercentageStat);
+        return (ConvertEnumToStat(statType, ref currentPStats, ref currentMiscStats), ConvertEnumToStat(statType, ref upgradedButNotAppliedPStats, ref upgradedButNotAppliedMiscStats));
 
-            // CANNON
-            case StatType.ProjectileDamage:
-                return (currentPStats.ProjectileDamageStat, upgradedButNotAppliedPStats.ProjectileDamageStat);
-            case StatType.CannonRechargeRate:
-                return (currentPStats.RechargeSecondsStat, upgradedButNotAppliedPStats.RechargeSecondsStat);
-            case StatType.ShotsPerFullCharge:
-                return (currentPStats.ShotsPerFullChargeStat, upgradedButNotAppliedPStats.ShotsPerFullChargeStat);
-            case StatType.OverheatForceCoolDown:
-                return (currentPStats.OverheatForceCoolDownStat, upgradedButNotAppliedPStats.OverheatForceCoolDownStat);
-
-            // MELEE
-            case StatType.MeleeDamage:
-                return (currentPStats.MeleeDamageStat, upgradedButNotAppliedPStats.MeleeDamageStat);
-            case StatType.MeleeDelay:
-                return (currentPStats.MeleeAttackDelayStat, upgradedButNotAppliedPStats.MeleeAttackDelayStat);
-            case StatType.BashDelay:
-                return (currentPStats.BashAttackDelayStat, upgradedButNotAppliedPStats.BashAttackDelayStat);
-            case StatType.EnemyStaggerTime:
-                return (currentPStats.MeleeStaggerTimeStat, upgradedButNotAppliedPStats.MeleeStaggerTimeStat);
-            case StatType.Reach:
-                return (currentPStats.MeleeReachStat, upgradedButNotAppliedPStats.MeleeReachStat);
-            case StatType.KnockbackForce:
-                return (currentPStats.BashForceStat, upgradedButNotAppliedPStats.BashForceStat);
-
-            // MISCELLANEOUS
-            case StatType.MaxScrapCarry:
-                return (currentMiscStats.MaxInventoryScrapStat, upgradedButNotAppliedMiscStats.MaxInventoryScrapStat);
-            case StatType.ItemCollectionRange:
-                return (currentMiscStats.MaxCollectionRangeStat, upgradedButNotAppliedMiscStats.MaxCollectionRangeStat);
-            case StatType.TimeLimit:
-                return (currentMiscStats.MaxLevelTimeStat, upgradedButNotAppliedMiscStats.MaxLevelTimeStat);
-            case StatType.CriticalChance:
-                return (currentMiscStats.CriticalHitChanceStat, upgradedButNotAppliedMiscStats.CriticalHitChanceStat);
-        }
-
-        return (null, null);
     }
 
     public void AddUpgradeOnce(StatType statType)
     {
-        switch (statType)
-        {
-            // STATS
-            case StatType.MaxHealth:
-                currentCost += UpgradeOnce(ref upgradedButNotAppliedPStats.MaxHealthStat);
-                break;
-            case StatType.RegenerationAmount:
-                currentCost += UpgradeOnce(ref upgradedButNotAppliedPStats.RegenerationAmountStat);
-                break;
-            case StatType.GroundSpeed:
-                currentCost += UpgradeOnce(ref upgradedButNotAppliedPStats.GroundSpeedStat);
-                break;
-            case StatType.SlideBoostForce:
-                currentCost += UpgradeOnce(ref upgradedButNotAppliedPStats.SlideBoostPercentageStat);
-                break;
-            case StatType.AirBoostForce:
-                currentCost += UpgradeOnce(ref upgradedButNotAppliedPStats.AirBoostPercentageStat);
-                break;
 
-            // CANNON
-            case StatType.ProjectileDamage:
-                currentCost += UpgradeOnce(ref upgradedButNotAppliedPStats.ProjectileDamageStat);
-                break;
-            case StatType.CannonRechargeRate:
-                currentCost += UpgradeOnce(ref upgradedButNotAppliedPStats.RechargeSecondsStat);
-                break;
-            case StatType.ShotsPerFullCharge:
-                currentCost += UpgradeOnce(ref upgradedButNotAppliedPStats.ShotsPerFullChargeStat);
-                break;
-            case StatType.OverheatForceCoolDown:
-                currentCost += UpgradeOnce(ref upgradedButNotAppliedPStats.OverheatForceCoolDownStat);
-                break;
+        currentCost += UpgradeOnce(ref ConvertEnumToStat(statType, ref upgradedButNotAppliedPStats, ref upgradedButNotAppliedMiscStats));
 
-            // MELEE
-            case StatType.MeleeDamage:
-                currentCost += UpgradeOnce(ref upgradedButNotAppliedPStats.MeleeDamageStat);
-                break;
-            case StatType.MeleeDelay:
-                currentCost += UpgradeOnce(ref upgradedButNotAppliedPStats.MeleeAttackDelayStat);
-                break;
-            case StatType.BashDelay:
-                currentCost += UpgradeOnce(ref upgradedButNotAppliedPStats.BashAttackDelayStat);
-                break;
-            case StatType.EnemyStaggerTime:
-                currentCost += UpgradeOnce(ref upgradedButNotAppliedPStats.MeleeStaggerTimeStat);
-                break;
-            case StatType.Reach:
-                currentCost += UpgradeOnce(ref upgradedButNotAppliedPStats.MeleeReachStat);
-                break;
-            case StatType.KnockbackForce:
-                currentCost += UpgradeOnce(ref upgradedButNotAppliedPStats.BashForceStat);
-                break;
-
-            // MISCELLANEOUS
-            case StatType.MaxScrapCarry:
-                currentCost += UpgradeOnce(ref upgradedButNotAppliedMiscStats.MaxInventoryScrapStat);
-                break;
-            case StatType.ItemCollectionRange:
-                currentCost += UpgradeOnce(ref upgradedButNotAppliedMiscStats.MaxCollectionRangeStat);
-                break;
-            case StatType.TimeLimit:
-                currentCost += UpgradeOnce(ref upgradedButNotAppliedMiscStats.MaxLevelTimeStat);
-                break;
-            case StatType.CriticalChance:
-                currentCost += UpgradeOnce(ref upgradedButNotAppliedMiscStats.CriticalHitChanceStat);
-                break;
-        }
 
         UpdateStatUI();
     }
 
     public void RemoveUpgradeOnce(StatType statType)
     {
-        switch (statType)
-        {
-            // STATS
-            case StatType.MaxHealth:
-                currentCost += RemoveUpgradeOnce(ref currentPStats.MaxHealthStat, ref upgradedButNotAppliedPStats.MaxHealthStat);
-                break;
-            case StatType.RegenerationAmount:
-                currentCost += RemoveUpgradeOnce(ref currentPStats.RegenerationAmountStat, ref upgradedButNotAppliedPStats.RegenerationAmountStat);
-                break;
-            case StatType.GroundSpeed:
-                currentCost += RemoveUpgradeOnce(ref currentPStats.GroundSpeedStat, ref upgradedButNotAppliedPStats.GroundSpeedStat);
-                break;
-            case StatType.SlideBoostForce:
-                currentCost += RemoveUpgradeOnce(ref currentPStats.SlideBoostPercentageStat, ref upgradedButNotAppliedPStats.SlideBoostPercentageStat);
-                break;
-            case StatType.AirBoostForce:
-                currentCost += RemoveUpgradeOnce(ref currentPStats.AirBoostPercentageStat, ref upgradedButNotAppliedPStats.AirBoostPercentageStat);
-                break;
+        currentCost += RemoveUpgradeOnce(ref ConvertEnumToStat(statType, ref currentPStats, ref currentMiscStats), ref ConvertEnumToStat(statType, ref upgradedButNotAppliedPStats, ref upgradedButNotAppliedMiscStats));
 
-            // CANNON
-            case StatType.ProjectileDamage:
-                currentCost += RemoveUpgradeOnce(ref currentPStats.ProjectileDamageStat, ref upgradedButNotAppliedPStats.ProjectileDamageStat);
-                break;
-            case StatType.CannonRechargeRate:
-                currentCost += RemoveUpgradeOnce(ref currentPStats.RechargeSecondsStat, ref upgradedButNotAppliedPStats.RechargeSecondsStat);
-                break;
-            case StatType.ShotsPerFullCharge:
-                currentCost += RemoveUpgradeOnce(ref currentPStats.ShotsPerFullChargeStat, ref upgradedButNotAppliedPStats.ShotsPerFullChargeStat);
-                break;
-            case StatType.OverheatForceCoolDown:
-                currentCost += RemoveUpgradeOnce(ref currentPStats.OverheatForceCoolDownStat, ref upgradedButNotAppliedPStats.OverheatForceCoolDownStat);
-                break;
-
-            // MELEE
-            case StatType.MeleeDamage:
-                currentCost += RemoveUpgradeOnce(ref currentPStats.MeleeDamageStat, ref upgradedButNotAppliedPStats.MeleeDamageStat);
-                break;
-            case StatType.MeleeDelay:
-                currentCost += RemoveUpgradeOnce(ref currentPStats.MeleeAttackDelayStat, ref upgradedButNotAppliedPStats.MeleeAttackDelayStat);
-                break;
-            case StatType.BashDelay:
-                currentCost += RemoveUpgradeOnce(ref currentPStats.BashAttackDelayStat, ref upgradedButNotAppliedPStats.BashAttackDelayStat);
-                break;
-            case StatType.EnemyStaggerTime:
-                currentCost += RemoveUpgradeOnce(ref currentPStats.MeleeStaggerTimeStat, ref upgradedButNotAppliedPStats.MeleeStaggerTimeStat);
-                break;
-            case StatType.Reach:
-                currentCost += RemoveUpgradeOnce(ref currentPStats.MeleeReachStat, ref upgradedButNotAppliedPStats.MeleeReachStat);
-                break;
-            case StatType.KnockbackForce:
-                currentCost += RemoveUpgradeOnce(ref currentPStats.BashForceStat, ref upgradedButNotAppliedPStats.BashForceStat);
-                break;
-
-            // MISCELLANEOUS
-            case StatType.MaxScrapCarry:
-                currentCost += RemoveUpgradeOnce(ref currentMiscStats.MaxInventoryScrapStat, ref upgradedButNotAppliedMiscStats.MaxInventoryScrapStat);
-                break;
-            case StatType.ItemCollectionRange:
-                currentCost += RemoveUpgradeOnce(ref currentMiscStats.MaxCollectionRangeStat, ref upgradedButNotAppliedMiscStats.MaxCollectionRangeStat);
-                break;
-            case StatType.TimeLimit:
-                currentCost += RemoveUpgradeOnce(ref currentMiscStats.MaxLevelTimeStat, ref upgradedButNotAppliedMiscStats.MaxLevelTimeStat);
-                break;
-            case StatType.CriticalChance:
-                currentCost += RemoveUpgradeOnce(ref currentMiscStats.CriticalHitChanceStat, ref upgradedButNotAppliedMiscStats.CriticalHitChanceStat);
-                break;
-        }
 
         UpdateStatUI();
     }
@@ -460,5 +283,52 @@ public class UpgradeMenuManager : MonoBehaviour
         menuManager.OpenMenu(mainMenuKey);
     }
 
+    public static ref UpgradablePlayerStat ConvertEnumToStat(StatType statType, ref PlayerStats pStats, ref MiscellaneousStats mStats)
+    {
+        switch (statType)
+        {
+            default:
+                throw new NullReferenceException("No valid stat for enum!");
+            case StatType.MaxHealth:
+                return ref pStats.MaxHealthStat;
+            case StatType.RegenerationAmount:
+                return ref pStats.RegenerationAmountStat;
+            case StatType.GroundSpeed:
+                return ref pStats.GroundSpeedStat;
+            case StatType.SlideBoostForce:
+                return ref pStats.SlideBoostPercentageStat;
+            case StatType.AirBoostForce:
+                return ref pStats.AirBoostPercentageStat;
+            case StatType.ProjectileDamage:
+                return ref pStats.ProjectileDamageStat;
+            case StatType.CannonRechargeRate:
+                return ref pStats.RechargeSecondsStat;
+            case StatType.ShotsPerFullCharge:
+                return ref pStats.ShotsPerFullChargeStat;
+            case StatType.OverheatForceCoolDown:
+                return ref pStats.OverheatForceCoolDownStat;
+            case StatType.MeleeDamage:
+                return ref pStats.MeleeDamageStat;
+            case StatType.MeleeDelay:
+                return ref pStats.MeleeAttackDelayStat;
+            case StatType.BashDelay:
+                return ref pStats.BashAttackDelayStat;
+            case StatType.EnemyStaggerTime:
+                return ref pStats.MeleeStaggerTimeStat;
+            case StatType.Reach:
+                return ref pStats.MeleeReachStat;
+            case StatType.KnockBackForce:
+                return ref pStats.BashForceStat;
+            case StatType.MaxScrapCarry:
+                return ref mStats.MaxInventoryScrapStat;
+            case StatType.ItemCollectionRange:
+                return ref mStats.MaxCollectionRangeStat;
+            case StatType.TimeLimit:
+                return ref mStats.MaxLevelTimeStat;
+            case StatType.CriticalChance:
+                return ref mStats.CriticalHitChanceStat;
+
+        }
+    }
 
 }
