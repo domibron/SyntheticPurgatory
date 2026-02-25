21d53bf0dc910aed4695d88f92db9f45fab025e9
domibron <dommcneill@outlook.com>
2026-02-25 16:22:06 +0000

    fixes to code for compile and removed releasing

--- Assets/Scripts/TOOLS/ForceGenerationButton.cs
+++ Assets/Scripts/TOOLS/ForceGenerationButton.cs
@@ -1,3 +1,5 @@
+# if UNITY_EDITOR
+
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -34,3 +36,5 @@ public class ForceSequenceButton : Editor
         DrawDefaultInspector();
     }
 }
+
+#endif
