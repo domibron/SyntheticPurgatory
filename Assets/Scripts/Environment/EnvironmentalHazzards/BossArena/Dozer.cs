baf3f54f9454687d7003d2cd37941c106fa11eb8
domibron <dommcneill@outlook.com>
2026-05-24 01:51:41 +0100

    Started comments on dozer

--- Assets/Scripts/Environment/EnvironmentalHazzards/BossArena/Dozer.cs
+++ Assets/Scripts/Environment/EnvironmentalHazzards/BossArena/Dozer.cs
@@ -5,34 +5,70 @@ using UnityEngine;
 // TODO: Rename this script, its a bad name.
 public class Dozer : MonoBehaviour
 {
+    // I really hate this script, too messy, too linked to other scripts, too depended on other things.
+    // TODO: refactor and adjust to follow some SOLID principles.
+    // Could have a event with a bool is left to delink most of this.
+
+    /// <summary>
+    /// The left gate to open and close.
+    /// </summary>
     [SerializeField]
     Door leftGate;
 
+    /// <summary>
+    /// The right gate to open and close.
+    /// </summary>
     [SerializeField]
     Door rightGate;
 
 
+    /// <summary>
+    /// The left monitor to flash or warn if the vehicle is spawning there. 
+    /// </summary>
     [SerializeField]
     WarningIndicator leftMonitor;
 
+    /// <summary>
+    /// The right monitor to flash or warn if the vehicle is spawning there. 
+    /// </summary>
     [SerializeField]
     WarningIndicator rightMonitor;
 
 
+    //TODO: Rename, bad names here, will leave since only used in arena.
+
+    // These basically are 2 points of the gate where the vehicles will randomly spawn in between to make sure all of the area is covered.
+    // This helps prevent the player just hugging the walls even though they still can.
+
     // LEFT
+
+    /// <summary>
+    /// The closest point of the left side spawn.
+    /// </summary>
     [SerializeField]
     Transform leftForkliftSpawnLocationOne;
 
+    /// <summary>
+    /// The furthest point of the left side spawn.
+    /// </summary>
     [SerializeField]
     Transform leftForkliftSpawnLocationTwo;
 
 
     // RIGHT
+
+    /// <summary>
+    /// The furthest point of the right side spawn.
+    /// </summary>
     [SerializeField]
     Transform rightForkliftSpawnLocationOne;
 
+    /// <summary>
+    /// The closest point of the right side spawn.
+    /// </summary>
     [SerializeField]
     Transform rightForkliftSpawnLocationTwo;
+
 
 
     [SerializeField]
@@ -42,32 +78,63 @@ public class Dozer : MonoBehaviour
     AudioSource rightAlarm;
 
 
+    /// <summary>
+    /// The vehicle will move back out of bounds to save spawning and destroying objects.
+    /// </summary>
     [SerializeField]
     Transform vehicleOutOfBounds;
 
-
+    /// <summary>
+    /// The collection of vehicles this event can move and use.
+    /// </summary>
     [SerializeField]
     GameObject[] vehicles;
 
+    /// <summary>
+    /// The current selected vehicle.
+    /// </summary>
     GameObject currentVehicle;
 
+    /// <summary>
+    /// The start point of the vehicle's path.
+    /// </summary>
     Vector3 startPos;
+
+    /// <summary>
+    /// The end point of the vehicle's path.
+    /// </summary>
     Vector3 endPos;
 
+    /// <summary>
+    /// How long to wait before letting the vehicle move across.
+    /// </summary>
     [SerializeField]
     private float waitTime = 3f;
 
+    /// <summary>
+    /// The speed at which the vehicle will travel at.
+    /// </summary>
     [SerializeField]
     private float speed = 0.3f;
 
+    /// <summary>
+    /// Is there already an attack happening.
+    /// </summary>
     private bool isAttacking = false;
 
+    /// <summary>
+    /// The droppable platforms between the gates.
+    /// </summary>
     [SerializeField]
     DroppablePlatform[] droppablePlatformsInPath;
 
     // private bool isOverridingGate = false;
 
     // public event Func<bool> OnJobCompleted;
+
+    /// <summary>
+    /// Called when the attack concludes.
+    /// </summary>
     public event Action OnJobCompleted;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -76,14 +143,10 @@ public class Dozer : MonoBehaviour
         ResetEverything();
     }
 
-    // Update is called once per frame
-    // void Update()
-    // {
-    //     // Debug-ing code
-    //     if (isAttacking) return;
-    //     StartCoroutine(DoDozerAttack());
-    // }
-
+    /// <summary>
+    /// Tries to start a attack.
+    /// </summary>
+    /// <returns><b>TRUE</b> if successful.</returns>
     public bool TryToStartAttack()
     {
         // TODO: Check if floor tiles are still intact.
@@ -93,6 +156,10 @@ public class Dozer : MonoBehaviour
         return true;
     }
 
+    /// <summary>
+    /// Checks to see if all the platforms are there and there is no container being dropped.
+    /// </summary>
+    /// <returns><b>TRUE</b> if nothing is stopping the attack.</returns>
     private bool CanDoAttack()
     {
         foreach (var platform in droppablePlatformsInPath)
@@ -100,7 +167,9 @@ public class Dozer : MonoBehaviour
             if (platform.HasDropped()) return false;
         }
 
-        Collider[] colliders = Physics.OverlapBox(transform.position + Vector3.up * 4f, new Vector3(40f, 8f, 8f));
+        // TODO: fix the bug that allows the dozer to attack when the container is being dropped.
+        // TODO: expensive call + phys layer is all.
+        Collider[] colliders = Physics.OverlapBox(transform.position + Vector3.up * 4f, new Vector3(40f, 8f, 8f), Quaternion.identity, Physics.AllLayers, QueryTriggerInteraction.Collide);
 
         if (colliders.Length <= 0) return true;
 
@@ -115,6 +184,7 @@ public class Dozer : MonoBehaviour
         return true;
 
     }
+
 
     private IEnumerator DoDozerAttack()
     {
