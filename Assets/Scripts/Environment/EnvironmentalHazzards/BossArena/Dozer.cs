4c23550efc4cd6595e9d115bfb1de197f949ad0f
domibron <dommcneill@outlook.com>
2026-05-26 01:37:26 +0100

    more comments

--- Assets/Scripts/Environment/EnvironmentalHazzards/BossArena/Dozer.cs
+++ Assets/Scripts/Environment/EnvironmentalHazzards/BossArena/Dozer.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using UnityEngine;
 
 // TODO: Rename this script, its a bad name.
+/// <summary>
+/// Managing of moving a random vehicle across a corridor and alerting the player and operating gates.
+/// </summary>
 public class Dozer : MonoBehaviour
 {
     // I really hate this script, too messy, too linked to other scripts, too depended on other things.
@@ -69,7 +72,7 @@ public class Dozer : MonoBehaviour
     [SerializeField]
     Transform rightForkliftSpawnLocationTwo;
 
-
+    // TODO: More stuff to delink.
 
     [SerializeField]
     AudioSource leftAlarm;
@@ -128,16 +131,13 @@ public class Dozer : MonoBehaviour
     [SerializeField]
     DroppablePlatform[] droppablePlatformsInPath;
 
-    // private bool isOverridingGate = false;
-
-    // public event Func<bool> OnJobCompleted;
 
     /// <summary>
     /// Called when the attack concludes.
     /// </summary>
     public event Action OnJobCompleted;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Start is called once before the first execution of Update after the MonoBehaviour is created ~ NO WAY!
     void Start()
     {
         ResetEverything();
@@ -185,24 +185,34 @@ public class Dozer : MonoBehaviour
 
     }
 
-
+    /// <summary>
+    /// The coroutine that actually does the attack.
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator DoDozerAttack()
     {
+        // Get everything ready for the attack.
         StartCoroutine(ReadyAttack(UnityEngine.Random.Range(0, 2) <= 0));
         yield return new WaitForEndOfFrame();
 
+        // check to make sure we have a valid vehicle.
         if (currentVehicle == null)
         {
-            Debug.LogError("Well shit, currentVehicle is null");
+            Debug.LogError("No valid vehicle, something went very wrong.", gameObject);
+            StartCoroutine(ResetAttack());
+            yield break; // exit out early.
         }
 
+        // Wait the specified time before moving the vehicle.
         yield return new WaitForSeconds(waitTime);
 
-
+        // Sync with frame time. (Could be in a state were delta time was double than normal.)
         yield return new WaitForEndOfFrame();
 
+        // local time for the event to base around of.
         float currentTime = 0f;
 
+        // Move the vehicle.
         while (currentTime < 1f)
         {
             yield return new WaitForEndOfFrame();
@@ -214,16 +224,21 @@ public class Dozer : MonoBehaviour
 
         yield return new WaitForEndOfFrame();
 
+        // end the attack.
         StartCoroutine(ResetAttack());
     }
 
+    /// <summary>
+    /// Coroutine that gets the attack ready, opening the gates, setting the alarm etc.
+    /// </summary>
+    /// <param name="isLeft">True will spawn the vehicle on the left side.</param>
+    /// <returns></returns>
     private IEnumerator ReadyAttack(bool isLeft)
     {
+        // Prevent any double calls.
         isAttacking = true;
 
-
-
-
+        // Spawn the vehicle.
         if (isLeft)
         {
             startPos = Vector3.Lerp(leftForkliftSpawnLocationOne.position, leftForkliftSpawnLocationTwo.position, UnityEngine.Random.Range(0f, 1f));
@@ -237,31 +252,43 @@ public class Dozer : MonoBehaviour
             endPos.z = leftForkliftSpawnLocationOne.position.z;
         }
 
+        // Assign the current vehicle.
         currentVehicle = vehicles[UnityEngine.Random.Range(0, vehicles.Length)];
 
+        // Rotate it to face the correct direction.
         if (!isLeft)
         {
             currentVehicle.transform.rotation = Quaternion.Euler(0, 180f, 0);
         }
 
+        // Move the vehicle to the correct position and make it visible and active.
         currentVehicle.transform.position = startPos;
         currentVehicle.SetActive(true);
 
+        // Wait for a moment. Not sure why. Maybe to make sure the vehicle is visible and ready?
         yield return new WaitForSeconds(0.5f);
 
+        // Open the gates, set the alarm and flash the screens.
         OpenGates();
         StartFlash(isLeft);
         PlayAlarm(isLeft);
     }
 
+    /// <summary>
+    /// The coroutine that resets the attack for the next one.
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator ResetAttack()
     {
+        // Turn off and close everything.
         CloseGates();
         TurnOffMonitors();
         StopAlarms();
 
+        // Wait for the gates to close.
         yield return new WaitForSeconds(0.7f);
 
+        // Remove the active vehicle. (Moving it out of bounds.)
         if (currentVehicle != null)
         {
             currentVehicle.SetActive(false);
@@ -269,12 +296,17 @@ public class Dozer : MonoBehaviour
             currentVehicle.transform.rotation = Quaternion.identity;
         }
 
+        // Reset the attack variables.
         currentVehicle = null;
         isAttacking = false;
 
+        // Let any scripts know we are finished.
         OnJobCompleted?.Invoke();
     }
 
+    /// <summary>
+    /// Resets everything skipping the wait. This does not reset the current vehicle's position and rotation.
+    /// </summary>
     private void ResetEverything()
     {
         CloseGates();
@@ -285,24 +317,37 @@ public class Dozer : MonoBehaviour
         // OnJobCompleted?.Invoke(); // ? maybe?
     }
 
+    /// <summary>
+    /// Open all the gates.
+    /// </summary>
     private void OpenGates()
     {
         leftGate.SetDoorState(true);
         rightGate.SetDoorState(true);
     }
 
+    /// <summary>
+    /// Close all the gates.
+    /// </summary>
     private void CloseGates()
     {
         leftGate.SetDoorState(false);
         rightGate.SetDoorState(false);
     }
 
+    /// <summary>
+    /// Turn of all the screens.
+    /// </summary>
     private void TurnOffMonitors()
     {
         leftMonitor.EndMonitor();
         rightMonitor.EndMonitor();
     }
 
+    /// <summary>
+    /// Flashes one screen with a warning symbol and the other blank.
+    /// </summary>
+    /// <param name="isLeftSide">True will put the warning on the left side monitor.</param>
     private void StartFlash(bool isLeftSide)
     {
         leftMonitor.ResetBGColor();
@@ -321,12 +366,19 @@ public class Dozer : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Stop all active alarms.
+    /// </summary>
     private void StopAlarms()
     {
         leftAlarm.Stop();
         rightAlarm.Stop();
     }
 
+    /// <summary>
+    /// Play the alarm sfx at the gate with the vehicle.
+    /// </summary>
+    /// <param name="isLeftSide">True will sound the alarm on the left side.</param>
     private void PlayAlarm(bool isLeftSide)
     {
         if (isLeftSide)
